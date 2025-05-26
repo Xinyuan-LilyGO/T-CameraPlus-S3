28fdd986276d6414c247434f492c18ea494ad137
LILYGO_L <13073925292@163.com>
2025-05-26 16:18:16 +0800

    Update "xiaozhi" firmware example

--- debug/examples/WIFI_HTTP_Download_File/WIFI_HTTP_Download_File.cpp
+++ debug/examples/WIFI_HTTP_Download_File/WIFI_HTTP_Download_File.cpp
@@ -4,11 +4,15 @@
  * @Author: None
  * @Date: 2024-06-08 16:18:12
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-07-11 15:52:30
+ * @LastEditTime: 2025-05-26 16:17:08
  * @License: GPL 3.0
  */
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "Arduino_DriveBus_Library.h"
+#include "pin_config.h"
+
+#define WIFI_CONNECT_WAIT_MAX (30 * 1000)
 
 // 替换以下信息为你的Wi-Fi网络信息
 // const char *ssid = "xinyuandianzi";
@@ -25,18 +29,156 @@ const char *fileDownloadUrl = "http://music.163.com/song/media/outer/url?id=2612
 
 static size_t CycleTime = 0;
 
+std::shared_ptr<Arduino_IIC_DriveBus> IIC_Bus =
+    std::make_shared<Arduino_HWIIC>(IIC_SDA, IIC_SCL, &Wire);
+
+std::unique_ptr<Arduino_IIC> SY6970(new Arduino_SY6970(IIC_Bus, SY6970_DEVICE_ADDRESS,
+                                                       DRIVEBUS_DEFAULT_VALUE, DRIVEBUS_DEFAULT_VALUE));
+
+void wifi_test(void)
+{
+    String text;
+    int wifi_num = 0;
+
+    Serial.println("\nScanning wifi");
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    WiFi.setTxPower(wifi_power_t::WIFI_POWER_19_5dBm);
+
+    wifi_num = WiFi.scanNetworks();
+    if (wifi_num == 0)
+    {
+        text = "\nWiFi scan complete !\nNo wifi discovered.\n";
+    }
+    else
+    {
+        text = "\nWiFi scan complete !\n";
+        text += wifi_num;
+        text += " wifi discovered.\n\n";
+
+        for (int i = 0; i < wifi_num; i++)
+        {
+            text += (i + 1);
+            text += ": ";
+            text += WiFi.SSID(i);
+            text += " (";
+            text += WiFi.RSSI(i);
+            text += ")";
+            text += (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " \n" : "*\n";
+            delay(10);
+        }
+    }
+
+    Serial.println(text);
+
+    text = "Connecting to ";
+    Serial.print("Connecting to ");
+    text += ssid;
+    text += "\n";
+
+    Serial.print(ssid);
+    WiFi.begin(ssid, password);
+
+    uint32_t last_tick = millis();
+    uint32_t i = 0;
+    bool is_smartconfig_connect = false;
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        text += ".";
+        delay(100);
+
+        if (millis() - last_tick > WIFI_CONNECT_WAIT_MAX)
+        { /* Automatically start smartconfig when connection times out */
+            text += "\nConnection timed out, start smartconfig";
+
+            is_smartconfig_connect = true;
+            WiFi.mode(WIFI_AP_STA);
+            Serial.println("\r\n wait for smartconfig....");
+            text += "\r\n wait for smartconfig....";
+            text += "\nPlease use #ff0000 EspTouch# Apps to connect to the distribution network";
+            WiFi.beginSmartConfig();
+
+            while (1)
+            {
+                if (WiFi.smartConfigDone())
+                {
+                    Serial.println("\r\nSmartConfig Success\r\n");
+                    Serial.printf("SSID:%s\r\n", WiFi.SSID().c_str());
+                    Serial.printf("PSW:%s\r\n", WiFi.psk().c_str());
+                    text += "\nSmartConfig Success";
+                    text += "\nSSID:";
+                    text += WiFi.SSID().c_str();
+                    text += "\nPSW:";
+                    text += WiFi.psk().c_str();
+                    last_tick = millis();
+                    break;
+                }
+            }
+        }
+    }
+
+    if (!is_smartconfig_connect)
+    {
+        text += "\nThe connection was successful ! \nTakes ";
+        Serial.print("\nThe connection was successful ! \nTakes ");
+        text += millis() - last_tick;
+        Serial.print(millis() - last_tick);
+        text += " ms\n";
+        Serial.println(" ms\n");
+    }
+}
+
 void setup()
 {
     Serial.begin(115200);
+    Serial.println("Ciallo");
 
-    // 连接到Wi-Fi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
+    while (SY6970->begin() == false)
     {
-        delay(1000);
-        Serial.println("Connecting to WiFi...");
+        Serial.println("SY6970 initialization fail");
+        delay(2000);
     }
-    Serial.println("Successfully connected to WiFi");
+    Serial.println("SY6970 initialization successfully");
+
+    // 开启ADC测量功能
+    while (SY6970->IIC_Write_Device_State(SY6970->Arduino_IIC_Power::Device::POWER_DEVICE_ADC_MEASURE,
+                                          SY6970->Arduino_IIC_Power::Device_State::POWER_DEVICE_ON) == false)
+    {
+        Serial.println("SY6970 ADC Measure ON fail");
+        delay(2000);
+    }
+    Serial.println("SY6970 ADC Measure ON successfully");
+
+    // 禁用看门狗定时器喂狗功能
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_WATCHDOG_TIMER, 0);
+    // 热调节阈值设置为60度
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_THERMAL_REGULATION_THRESHOLD, 60);
+    // 充电目标电压电压设置为4224mV
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_CHARGING_TARGET_VOLTAGE_LIMIT, 4224);
+    // 最小系统电压限制为3600mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_MINIMUM_SYSTEM_VOLTAGE_LIMIT, 3600);
+    // 设置OTG电压为5062mV
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_OTG_VOLTAGE_LIMIT, 5062);
+    // 输入电流限制设置为3250mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_INPUT_CURRENT_LIMIT, 3250);
+    // 快速充电电流限制设置为2112mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_FAST_CHARGING_CURRENT_LIMIT, 2112);
+    // 预充电电流限制设置为192mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_PRECHARGE_CHARGING_CURRENT_LIMIT, 192);
+    // 终端充电电流限制设置为320mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_TERMINATION_CHARGING_CURRENT_LIMIT, 320);
+    // OTG电流限制设置为500mA
+    SY6970->IIC_Write_Device_Value(SY6970->Arduino_IIC_Power::Device_Value::POWER_DEVICE_OTG_CHARGING_LIMIT, 500);
+}
+
+void loop()
+{
+    wifi_test();
+
+    Serial.println("\nhttp wifi test");
 
     // 初始化HTTP客户端
     HTTPClient http;
@@ -117,9 +259,6 @@ void setup()
         Serial.printf("Failed to download\n");
         Serial.printf("Error httpCode: %d \n", httpCode);
     }
-}
 
-void loop()
-{
-    // 这里不需要执行任何操作，因为所有的工作都在setup函数中完成
+    delay(1000);
 }
